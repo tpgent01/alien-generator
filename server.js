b21c81d66d991cbe53f47c288f99e257332ba3cf
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-24 20:10:46 -0500

    added new secret

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'secret things',
+  secret: process.env.SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
