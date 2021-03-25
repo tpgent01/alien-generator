76003e785ada0802d30d4431631b0772ad438581
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-03-24 20:20:17 -0500

    corrected seeds

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -4,17 +4,17 @@ const commentdata = [
   {
     comment_text: 'Nunc rhoncus dui vel sem.',
     user_id: 1,
-    post_id: 1
+    alien_id: 1
   },
   {
     comment_text: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
     user_id: 1,
-    post_id: 2
+    alien_id: 2
   },
   {
     comment_text: 'Aliquam erat volutpat. In congue.',
     user_id: 3,
-    post_id: 3
+    alien_id: 3
   }
 ];
 
