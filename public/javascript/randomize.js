0f0387b4656269935dd0b24cc151a07995942c10
J Scott Rumptz <jscott@rumptz.tech>
2021-03-26 23:24:52 -0500

    dashboard functionality completed. can now generate and save lifeforms

--- public/javascript/randomize.js
+++ public/javascript/randomize.js
@@ -1,26 +1,40 @@
+let lifeformAbilitiesArray = [];
+
 const randomize = () => {
-    const randomName = document.getElementById('randomName')
-    const randomBackstory = document.getElementById('randomBackstory')
-    const randomLocomotion = document.getElementById('randomLocomotion')
-    const randomUpper = document.getElementById('randomUpper')
-    const randomHead = document.getElementById('randomHead')
-    const randomFace = document.getElementById('randomFace')
-    const randomMouth = document.getElementById('randomMouth')
-    const randomEyes = document.getElementById('randomEyes')
-    const randomNose = document.getElementById('randomNose')
-    const randomEars = document.getElementById('randomEars')
-    const randomSkin = document.getElementById('randomSkin')
-    const randomOther = document.getElementById('randomOther')
-    const randomAbilities = document.getElementById('randomAbilities')
+    const randomName = document.getElementById('randomName');
+    const randomDisposition = document.getElementById('randomDisposition');
+    const randomBirth = document.getElementById('randomBirth');
+    const randomReason = document.getElementById('randomReason');
+    const randomLocomotion = document.getElementById('randomLocomotion');
+    const randomUpper = document.getElementById('randomUpper');
+    const randomHead = document.getElementById('randomHead');
+    const randomFace = document.getElementById('randomFace');
+    const randomMouth = document.getElementById('randomMouth');
+    const randomEyes = document.getElementById('randomEyes');
+    const randomNose = document.getElementById('randomNose');
+    const randomEars = document.getElementById('randomEars');
+    const randomSkin = document.getElementById('randomSkin');
+    const randomOther = document.getElementById('randomOther');
+    const randomAbilities = document.getElementById('randomAbilities');
+    const generateLifeform = document.getElementById('generateLifeform');
+    const saveLifeform = document.getElementById('saveLifeform');
 
     randomName.addEventListener('click', (event) => {
         event.preventDefault();
         document.getElementById('lifeformName').value = '';
         getRandom('lifeformName','name');
     })
-    randomBackstory.addEventListener('click', (event) => {
+    randomDisposition.addEventListener('click', (event) => {
         event.preventDefault();
-        getRandom('lifeformBackstory','backstory');
+        getRandom('lifeformDisposition','disposition');
+    })
+    randomBirth.addEventListener('click', (event) => {
+        event.preventDefault();
+        getRandom('lifeformBirth','birth');
+    })
+    randomReason.addEventListener('click', (event) => {
+        event.preventDefault();
+        getRandom('lifeformReason','reason');
     })
     randomLocomotion.addEventListener('click', (event) => {
         event.preventDefault();
@@ -64,8 +78,19 @@ const randomize = () => {
     })
     randomAbilities.addEventListener('click', (event) => {
         event.preventDefault();
-        getRandom('lifeformAbilities','abilities');
+        getRandomAbilities();
     })
+    generateLifeform.addEventListener('click', (event) => {
+        event.preventDefault();
+        getLifeform();
+    })
+
+    saveLifeform.addEventListener('click', (event) => {
+        event.preventDefault();
+        saveAlienLifeform();
+    })
+
+    getLifeform();
 }
 
 const getRandom = (place, item) => {
@@ -75,6 +100,27 @@ const getRandom = (place, item) => {
             .then(function(data) {
                     document.getElementById(place).value = data[item];
                     getPhysicalDescription();
+                    getBackstory();
+                })
+            .catch(function(error) {
+                    console.log(error);
+                });
+}
+
+const getRandomAbilities = () => {
+    named = document.getElementById('lifeformName').value
+    return fetch(`/api/generate/alien/${named}`)
+            .then(response => response.json())
+            .then(function(data) {
+                document.getElementById('lifeformAbilities').value = ``;
+                data.abilities.forEach(ability=> {
+                    document.getElementById('lifeformAbilities').value = document.getElementById('lifeformAbilities').value + `${ability.name} - ${ability.description}
+
+`
+                });
+                getPhysicalDescription();
+                getBackstory();
+                lifeformAbilitiesArray = data.abilities;
                 })
             .catch(function(error) {
                     console.log(error);
@@ -95,7 +141,15 @@ const getPhysicalDescription = () => {
       }'s epidermis is ${document.getElementById('lifeformSkin').value}, but most notably ${
         document.getElementById('lifeformName').value
       } has ${document.getElementById('lifeformOther').value}.`
+    
+    getBackstory();
+    document.getElementById('name').value = document.getElementById('lifeformName').value
+    document.getElementById('abilities').value = document.getElementById('lifeformAbilities').value
 }
+
+const getBackstory = () => {
+    document.getElementById('backstory').value = `${document.getElementById('lifeformName').value} ${document.getElementById('lifeformDisposition').value} It ${document.getElementById('lifeformBirth').value}. ${document.getElementById('lifeformName').value} ${document.getElementById('lifeformReason').value}`;
+  }
 
 const generateUpperMadLib = () => {
     let chance = Math.floor(Math.random() * Math.floor(6)) + 1;
@@ -108,7 +162,7 @@ const generateUpperMadLib = () => {
     }
   }
 
-  const generateFaceMadLib = () => {
+const generateFaceMadLib = () => {
     let chance = Math.floor(Math.random() * Math.floor(6)) + 1;
     if (chance >= 1 && chance <= 2) {
       return `This does nothing to distract one from`;
@@ -116,6 +170,88 @@ const generateUpperMadLib = () => {
       return `Despite this, you can't help but notice`;
     } else {
       return `As if all of that wasn't odd enough, you now take in`;
+    }
+  }
+
+const getLifeform = () => {
+    return fetch(`/api/generate/alien/`)
+            .then(response => response.json())
+            .then(function(data) {
+                document.getElementById('lifeformName').value = data.name;
+                document.getElementById('lifeformDisposition').value = data.disposition;
+                document.getElementById('lifeformBirth').value = data.birth;
+                document.getElementById('lifeformReason').value = data.reason;
+                document.getElementById('lifeformLocomotion').value = data.locomotion;
+                document.getElementById('lifeformUpper').value = data.upperAppendageType;
+                document.getElementById('lifeformHead').value = data.head;
+                document.getElementById('lifeformFace').value = data.facialFeatures;
+                document.getElementById('lifeformMouth').value = data.mouth;
+                document.getElementById('lifeformEyes').value = data.eyes;
+                document.getElementById('lifeformNose').value = data.nose;
+                document.getElementById('lifeformEars').value = data.ears;
+                document.getElementById('lifeformSkin').value = data.skin;
+                document.getElementById('lifeformOther').value = data.otherFeatures;
+                document.getElementById('lifeformAbilities').value = getRandomAbilities();                
+
+                    getPhysicalDescription();
+                    getBackstory();
+                })
+            .catch(function(error) {
+                    console.log(error);
+                });
+  }
+
+async function saveAlienLifeform() {
+    const name = document.getElementById('lifeformName').value;
+    const disposition = document.getElementById('lifeformDisposition').value;
+    const birth = document.getElementById('lifeformBirth').value;
+    const reason = document.getElementById('lifeformReason').value;
+    const locomotion = document.getElementById('lifeformLocomotion').value;
+    const upperAppendageType = document.getElementById('lifeformUpper').value;
+    const head = document.getElementById('lifeformHead').value;
+    const facialFeatures = document.getElementById('lifeformFace').value;
+    const mouth = document.getElementById('lifeformMouth').value;
+    const eyes = document.getElementById('lifeformEyes').value;
+    const nose = document.getElementById('lifeformNose').value;
+    const ears = document.getElementById('lifeformEars').value;
+    const skin = document.getElementById('lifeformSkin').value;
+    const otherFeatures = document.getElementById('lifeformOther').value;
+    const backstory = document.getElementById('backstory').value;
+    const physicalDescription = document.getElementById('physicalDescription').value;
+    const abilities = lifeformAbilitiesArray
+
+
+  
+    const response = await fetch(`/api/aliens`, {
+      method: 'POST',
+      body: JSON.stringify({
+        lifeform:{name,
+        disposition,
+        birth,
+        reason,
+        backstory,
+        locomotion,
+        upperAppendageType,
+        head,
+        facialFeatures,
+        mouth,
+        eyes,
+        nose,
+        ears,
+        skin,
+        otherFeatures,
+        physicalDescription,
+        abilities}
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
     }
   }
 
