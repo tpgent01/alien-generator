76d6af027c9ae5da0174a9671612dcfd7cb57121
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-03-29 14:28:07 -0500

    added basic comment js structure - need to change querySelector based on view ejs details template

---
+++ public/javascript/comment.js
@@ -0,0 +1,31 @@
+async function commentHandler(event) {
+    event.preventDefault();
+  
+    const comment_text = document.querySelector(
+        // 'textarea[name="comment-body"]'
+        ).value.trim();
+    const alien_id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+    if (comment_text) {
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+          alien_id,
+          comment_text
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+    }
+}
+  
+document.querySelector('.comment').addEventListener('submit', commentHandler);
