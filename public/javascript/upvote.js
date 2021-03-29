8709630df1d8dfd39bfc6b96b08048de75c47db6
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-03-29 14:45:51 -0500

    changed url to fetch

--- public/javascript/upvote.js
+++ public/javascript/upvote.js
@@ -5,7 +5,7 @@ async function upvoteClickHandler(event) {
         window.location.toString().split('/').length - 1
     ];
 
-    const response = await fetch('/api/alien/upvote', {
+    const response = await fetch('/api/alien/:id', {
         method: 'PUT',
         body: JSON.stringify({
             post_id: id
